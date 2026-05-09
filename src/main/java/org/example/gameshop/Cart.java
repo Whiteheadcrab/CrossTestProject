551200a3292490dc92d5073b234f03579002c5d7
Mykyta Bieliaiev <bieliaiev.mykyta@gmail.com>
2026-05-09 20:48:50 +0200

    Version 0.1 - Added comments

--- src/main/java/org/example/gameshop/Cart.java
+++ src/main/java/org/example/gameshop/Cart.java
@@ -8,24 +8,29 @@ import java.util.Collections;
 import java.util.List;
 
 public class Cart {
+    //Initialize list for games
     private final List<Game> listGames = new ArrayList<>();
 
 
+    //Add game to list
     public void addGame(Game game)
     {
         listGames.add(game);
     }
 
+    //Remove game to list
     public void removeGame(Game game)
     {
         listGames.remove(game);
     }
 
+    //Show list of game
     public List<Game> getItems()
     {
         return Collections.unmodifiableList(listGames);
     }
 
+    //Show total price for list
     public BigDecimal getTotalPrice()
     {
         BigDecimal totalPrice = BigDecimal.ZERO ;
