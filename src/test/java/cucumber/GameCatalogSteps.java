9d42ae6d2dd6b3b4016e9ff1ab433cc57f256082
Mykyta Bieliaiev <bieliaiev.mykyta@gmail.com>
2026-05-12 11:14:17 +0200

    Version 0.3 - Moved steps implementation

--- src/test/java/cucumber/GameCatalogSteps.java
+++ src/test/java/cucumber/GameCatalogSteps.java
@@ -1,4 +1,21 @@
 package cucumber;
 
+import io.cucumber.java.en.When;
+import org.gameshop.GameCatalog;
+
 public class GameCatalogSteps {
+    //Created object for Game catalog
+    private final GameCatalog gameCatalog = new GameCatalog();
+
+    @When("I search game by id {int}")
+    public void searchGameById(int id) {
+    }
+
+    @When("I search game by name {string}")
+    public void searchGameByName(String name) {
+    }
+
+    @When("I search games by game's category {string}")
+    public void searchGameByGameCategory(String gameCategory) {
+    }
 }
