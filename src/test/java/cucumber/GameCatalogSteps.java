935d91cd559f573d00f0c4d14280ccce6a77a7c6
Mykyta Bieliaiev <bieliaiev.mykyta@gmail.com>
2026-05-12 15:55:20 +0200

    Version 0.3 - Started writing implementation for cucumber steps

--- src/test/java/cucumber/GameCatalogSteps.java
+++ src/test/java/cucumber/GameCatalogSteps.java
@@ -1,21 +1,59 @@
 package cucumber;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import org.gameshop.Game;
 import org.gameshop.GameCatalog;
+
+import java.util.List;
+import java.util.Map;
 
 public class GameCatalogSteps {
     //Created object for Game catalog
     private final GameCatalog gameCatalog = new GameCatalog();
+    //Created object for game
+    private Game foundGame;
+    //Create Map for inserted parameters
+    private Map<String, String> parameters;
+
+    //Created for setting up parameters used for testing
+    @Given("Set parameter")
+    public void setParameter(DataTable dataTable) {
+        parameters = dataTable.asMaps().get(0);
+    }
 
     @When("I search game by id {int}")
-    public void searchGameById(int id) {
+    public Game searchGameById(int id) {
+        foundGame =  gameCatalog.findById(id);
+        return foundGame;
     }
 
     @When("I search game by name {string}")
-    public void searchGameByName(String name) {
+    public Game searchGameByName(String name) {
+        foundGame = gameCatalog.findByName(name);
+        return foundGame;
     }
 
     @When("I search games by game's category {string}")
-    public void searchGameByGameCategory(String gameCategory) {
+    public List<Game> searchGameByGameCategory(String gameCategory) {
+        return gameCatalog.findByCategory(Game.Category.valueOf(gameCategory));
+    }
+
+    @Then("I verify found game is {string}")
+    public void verifyFoundGame(String expectedGameName) {
+        if (!foundGame.name().equals(expectedGameName)) {
+            throw new AssertionError("Expected " + expectedGameName + ", but found " + foundGame.name());
+        }
+    }
+
+    @Then("I verify found game all data similar to {game}")
+    public void verifyFoundGameData(Game expectedGame) {
+        if (!foundGame.equals(expectedGame)) {
+            throw new AssertionError("Expected and actual game data are not equal" +
+                    "\n" + "Expected data -" + expectedGame +
+                    "\n" + "Actual data -" + foundGame);
+        }
     }
 }
