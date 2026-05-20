4aca21cbf0f5341f30431d9934df1aa373f41662
Mykyta Bieliaiev <bieliaiev.mykyta@gmail.com>
2026-05-20 14:22:01 +0200

    Version 0.3 - updated runner for using specific tags

--- src/test/java/cucumber/RunCucumberTest.java
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -1,7 +1,15 @@
 package cucumber;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-@Cucumber
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "summary"},
+        tags = "@Cucumber"
+        // Example of tags for total , feature and test
+        // "@Cucumber", "@GameCatalog" , "@id\\(GameCatalog_searchByName_1\\)"
+)
 public class RunCucumberTest {
 }
