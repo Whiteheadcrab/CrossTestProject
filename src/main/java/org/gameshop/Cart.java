9008399136f4dc512071d37296abd37d1aa93241
Mykyta Bieliaiev <bieliaiev.mykyta@gmail.com>
2026-05-23 20:53:15 +0200

    Update cart behavior

--- src/main/java/org/gameshop/Cart.java
+++ src/main/java/org/gameshop/Cart.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Cart {
     //Initialize list for games
@@ -11,26 +12,58 @@ public class Cart {
 
 
     //Add game to list
-    public void addGame(Game game)
-    {
+    public void addGame(Game game) {
+        //Not found game for adding
+        if (game == null) {
+            throw new NoSuchElementException("Game was not found");
+        }
+
+        //Check that game is not added already to the list
+        boolean gameAlreadyExists = listGames.stream()
+                .anyMatch(cartGame -> cartGame.id() == game.id());
+        if (gameAlreadyExists) {
+            throw new IllegalArgumentException("Game with id " + game.id() + " already exists in cart");
+        }
+
+        //Add game
         listGames.add(game);
     }
 
     //Remove game to list
-    public void removeGame(Game game)
-    {
-        listGames.remove(game);
+    public void removeGame(Game game) {
+        //Not found game for removing
+        if (game == null) {
+            throw new NoSuchElementException("Game was not found in cart");
+        }
+
+        //If possible to remove - value will be true and item to be removed
+        boolean gameWasRemoved = listGames.removeIf(cartGame -> cartGame.id() == game.id());
+
+        //If boolean false - show exception
+        if (!gameWasRemoved) {
+            throw new NoSuchElementException("Game with id " + game.id() + " was not found in cart");
+        }
     }
 
     //Show list of game
     public List<Game> getItems()
     {
+        //Verify if cart is empty
+        if (listGames == null || listGames.isEmpty()) {
+            throw new NoSuchElementException("Cart is empty");
+        }
+
         return Collections.unmodifiableList(listGames);
     }
 
     //Show total price for list
     public BigDecimal getTotalPrice()
     {
+        //Verify if cart is empty
+        if (listGames == null || listGames.isEmpty()) {
+            throw new NoSuchElementException("Cart is empty");
+        }
+
         BigDecimal totalPrice = BigDecimal.ZERO ;
 
         for (Game game : listGames)
@@ -38,5 +71,17 @@ public class Cart {
             totalPrice = totalPrice.add(game.price());
         }
         return totalPrice;
+    }
+
+    //Empty cart
+    public void emptyCart()
+    {
+        listGames.clear();
+    }
+
+    //Pay for games in cart - will be implemented in future
+    public void payForCart()
+    {
+        throw new UnsupportedOperationException("Payment for cart will be implemented in future");
     }
 }
