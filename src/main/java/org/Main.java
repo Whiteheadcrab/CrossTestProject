011fc0dfa0750c8c204cbd04dd261426098b662f
Mykyta Bieliaiev <bieliaiev.mykyta@gmail.com>
2026-06-06 09:46:11 +0200

    Version 0.4.2 - Rest API Migration - refactoring Main with SpringBoot

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,17 +1,11 @@
 package org;
 
-import org.gameshop.Game;
-import org.gameshop.Functions.GameCatalogFunctions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        //Create variable for game list
-        GameCatalogFunctions gameCatalog = new GameCatalogFunctions();
-
-        //Actions per each item in game list
-        for (Game game : gameCatalog.getAllGames()) {
-            System.out.println(game.id() + ". " + game.name() + " - " + game.categories() + " - costs - " + game.price() + " PLN");
-        }
+        SpringApplication.run(Main.class, args);
     }
 }
